502e6d0976e153bb9545ec2a7cded6f1f50eec6d
Ghost543 <petermuwanguzi51@gmail.com>
2022-09-28 16:07:42 +0300

    config added

--- src/startup.rs
+++ src/startup.rs
@@ -1,11 +1,12 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgConnection;
 use std::net::TcpListener;
 
 // use email_news_subscription::routes;
 // use crate::routes::{health_check, subscriptions};
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route(
